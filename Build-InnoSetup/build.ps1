89abec2b7199f4ac2f6c1aa7ed6faad97f31f50a
Jens Brand <jens.brand@wolf-software.de>
2021-11-01 16:42:07 +0100

    Build und Install fertig, inklusive SignTool

--- Build-InnoSetup/build.ps1
+++ Build-InnoSetup/build.ps1
@@ -5,21 +5,80 @@ param()
 # https://github.com/Microsoft/vsts-task-lib
 Trace-VstsEnteringInvocation $MyInvocation
 
+<#Lädt ein securefile vom TFS heunter#>
+<#Siehe auch: https://stackoverflow.com/a/54317270/5707462#>
+function Get-SecureFile {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$FileId
+    )
+
+    $secTicket = Get-VstsSecureFileTicket -Id $FileId
+    $secName = Get-VstsSecureFileName -Id $FileId
+    $tempDirectory = Get-VstsTaskVariable -Name "Agent.TempDirectory" -Require
+    $collectionUrl = Get-VstsTaskVariable -Name "System.TeamFoundationCollectionUri" -Require
+    $project = Get-VstsTaskVariable -Name "System.TeamProject" -Require
+    $filePath = Join-Path $tempDirectory $secName
+
+    $token = Get-VstsTaskVariable -Name "System.AccessToken" -Require
+    $user = Get-VstsTaskVariable -Name "Build.QueuedById" -Require
+
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $User, $token)))
+    $headers = @{
+        Authorization = ("Basic {0}" -f $base64AuthInfo)
+        Accept        = "application/octet-stream"
+    } 
+
+    $url = "$($collectionUrl)$project/_apis/distributedtask/securefiles/$($FileId)?ticket=$($secTicket)&download=true&api-version=5.0-preview.1"
+
+    try {
+        $response = Invoke-WebRequest -Uri $url -Method Get -Headers $headers -OutFile $filePath
+        if ($response.StatusCode -lt 300) {
+            Write-Host $response
+            return $filePath
+        }
+        else {
+            Write-Host $response.StatusCode
+            Write-Host $response.StatusDescription
+            return ""
+        }
+    }
+    catch {
+        Write-Host $_.Exception.Response.StatusDescription
+        return ""
+    }
+
+}
 
 [string]$version = Get-VstsInput -Name version
 [string]$file = Get-VstsInput -Name file
+[string]$secFileId = Get-VstsInput -Name secureFileId
+[string]$signCertPassword = Get-VstsInput -Name signCertPassword
+[string]$timeServer = Get-VstsInput -Name timeServer
+[string]$signToolName = Get-VstsInput -Name signToolName
+
+if ([string]::IsNullOrEmpty($secFileId)) {
+    Write-Host "No secure file configured"
+    $configureSignTool = $false
+}
+else {
+    $secureFilePath = Get-SecureFile -FileId $secFileId
+    $configureSignTool = $true
+    Write-Host "Using secure file " $secureFilePath
+}
+
 
 $toolDirectory = Join-Path $env:AGENT_TOOLSDIRECTORY "\InnoSetup\$version\"
 $innoCompiler = $toolDirectory + "ISCC.exe"
 
-if (!(test-path -path $innoCompiler))
-{
+if (!(test-path -path $innoCompiler)) {
     throw "Inno Setup not installed!"
 }
 
 $extn = [IO.Path]::GetExtension($file)
-if ($extn  -ne ".iss")
-{
+if ($extn -ne ".iss") {
     throw "Not .iss file extension!"
 }
 
@@ -28,12 +87,26 @@ Write-Host "Building..."
 Write-Host "Compiler  =>" $innoCompiler
 Write-Host "File .iss =>" $file
 
-Start-Process -FilePath $innoCompiler $file -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+if ($configureSignTool) {
+    $signTool = [string]::Format('/S{0}="signtool.exe sign /as /fd sha256 /f $q{1}$q /p $q{2}$q /tr $q{3}$q /td sha256 $f"', $signToolName, $secureFilePath, $signCertPassword, $timeServer)
+
+    Write-Host "Starting Inno Setup compiler: " $innoCompiler $signTool $file
+    Start-Process -FilePath $innoCompiler -ArgumentList $signTool, $file -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+}
+else {
+    Write-Host "Starting Inno Setup compiler: " $innoCompiler $file
+    Start-Process -FilePath $innoCompiler $file -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+}
 
 $(Get-Content stdout.txt)
 
 $erro = $(Get-Content stderr.txt)
-if ($erro){
+if ($erro) {
     throw $erro
 }	
 Write-Host "Process completed successfully!"-ForegroundColor Green
+
+if (-not [string]::IsNullOrEmpty($secureFilePath)) {
+    Write-Host "Erasing downloaded secure file: " + $secureFilePath
+    Remove-Item -Path $secureFilePath 
+}
--- readme.md
+++ readme.md
@@ -6,13 +6,19 @@ Tools, that help you to build Inno Setup scripts during build pipeline.
 
 Acquires a specific version of Inno Setup from the internet and adds it to the PATH. Use this task to install Inno Setup for subsequent tasks.
 
-* **Version** - Version that should be installed. You can find a complete list [here](https://jrsoftware.org/isdlold.php). Version is required.
+* **version** - Version that should be installed. You can find a complete list [here](https://jrsoftware.org/isdlold.php). Version is required.
 
 # Build Task
 
-This build task compile an Inno Setup script file (*.iss).
+This build task compiles an Inno Setup script file (*.iss). Optionally it can sign the installer (and also the uninstaller) with a digital certificate. When Setup has a valid digital signature, users will not see an "unidentified program" warning when launching it.
 
-* **Version** - Version cached in Installer task. Version is required.
+* **version** - Version cached in Installer task. Version is required.
+* **file** - File to compile (*.iss)*). File is required.
 
-* **File** - File to compile (*.iss)*).
+The following Parameters are only required, if you wish to sign your installer with a digital certificate.
+
+* **secureFileId** - This is the name of the secure file which contains the authenticode certificate to use. 
+* **signCertPassword** - The password for the certificate file.
+* **timeServer** - Server-Url for timestamp-server. Example: http://timestamp.comodoca.com/?td=sha256. 
+* **signToolName** - This is the name of the Inno Setup signtool to define. You must define this name in your setup-script with the parameter [Setup]: SignTool (see: https://jrsoftware.org/ishelp/topic_setup_signtool.htm) and the name must be identical to this one.
 
